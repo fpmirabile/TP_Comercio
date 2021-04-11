a59c889dc0fc38bb78e3bd63e0346c67dca476ef
Fernando Mirabile <fpmirabile@gmail.com>
2021-04-11 12:31:08 -0300

    Agrego react router dom

---
+++ src/store/error-pages/not-found/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+class NotFound extends React.PureComponent {
+    render() {
+        return (
+            <div>Not found</div>
+        )
+    }
+}
+
+export default NotFound;
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,14 +1,24 @@
 import * as React from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from "./header";
 import PageContent from "./content";
+import NotFound from "./error-pages/not-found";
 
 class Store extends React.PureComponent {
   render() {
     return (
-      <div>
+      // Browser router porque queremos las url identicas sin #
+      <BrowserRouter>
         <Header />
-        <PageContent />
-      </div>
+        <Switch>
+          <Route path="/" exact>
+            <PageContent />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
